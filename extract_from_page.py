5083f31bf97b206431aa766cd0868728a123f4ab
n8x0b7T <deretilium@protonmail.com>
2022-02-15 16:00:35 -0500

    headers

--- extract_from_page.py
+++ extract_from_page.py
@@ -2,15 +2,25 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-req_headers = {'User-Agent': 'Mozilla/5.0'}
+req_headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0',
+    'Accept': '*/*',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'X-Requested-With': 'XMLHttpRequest',
+    'DNT': '1',
+    'Connection': 'keep-alive',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'TE': 'trailers',
+}
 
 f = csv.reader(open("a.csv"), delimiter='\t')
-urls = []
 
+urls = []
 for i in f:
     urls.append(i[-1])
-
-print(urls)
 
 def parse_site(url):
     r = requests.get(url, headers=req_headers).text
