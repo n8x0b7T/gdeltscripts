915e1f52a82a7f85bbcf28cccb5659bfba6c7dd1
n8x0b7T <deretilium@protonmail.com>
2022-02-15 15:22:21 -0500

    single quotes

--- extract_from_page.py
+++ extract_from_page.py
@@ -1,8 +1,26 @@
 import requests
+from bs4 import BeautifulSoup
 
-url = "https://www.itespresso.fr/press-release/sectigo-annonce-lvnement-exclusif-du-secteur-de-la-cyberscurit-le-sommet-identity-first-security-2022-de-sectigo"
+url = 'https://www.itespresso.fr/press-release/sectigo-annonce-lvnement-exclusif-du-secteur-de-la-cyberscurit-le-sommet-identity-first-security-2022-de-sectigo'
 
 req_headers = {'User-Agent': 'Mozilla/5.0'}
 
 
-r = requests.get(url, headers=req_headers)
+def parse_site(url):
+    r = requests.get(url, headers=req_headers).text
+
+    soup = BeautifulSoup(r, 'html.parser')
+    text = soup.find_all(text=True)
+
+    x= {
+        'title': soup.find('title'),
+        'body': ''
+    }
+
+    for i in text:
+        if i.parent.name == 'p':
+            x.body += f'{i} '
+
+    return x
+
+print(parse_site(url))
