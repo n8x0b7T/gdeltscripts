e662579493c96ed26e64b7d1872bad23004236c8
n8x0b7T <deretilium@protonmail.com>
2022-04-20 14:09:25 -0400

    download v1

---
+++ dlarchivesv1.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import argparse
+import requests
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-u', '--update', help='update the database',  action='store_true')    
+parser.add_argument(
+    '-o', help='update the database',  action='store_true', default="./archivesV1/")
+args = parser.parse_args()
+
+master_list = 'http://data.gdeltproject.org/events/index.html'
+local_database = 'master_file_listV1.txt'
+
+os.makedirs(args.o, exist_ok=True)
+
+
+def download_file(url):
+    print('Updating local database')
+    r = requests.get(url, stream=True)
+    with open(local_database, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+                f.flush()
+
+
+if args.update:
+    download_file(master_list)
+
+
+def get_csv(url):
+    url = "http://data.gdeltproject.org/events/" + url
+    r = requests.get(url)
+    with open(args.o + url.split('/')[-1], 'wb') as f:
+        f.write(r.content)
+
+def splitv1(x):
+    return x.split('HREF="')[1].split('"')[0]
+
+def download_archives():
+    urls = []
+    with open(local_database, 'r') as f:
+        for i in f.readlines()[1:-1]:
+            url = splitv1(i)
+            if 'export' in url:
+                urls.append(url)
+
+    for idx, val in enumerate(urls):
+        if not os.path.isfile(args.o + val.split('/')[-1]):
+            print(f'Downloading {idx+1}/{len(urls)} archives', end='\r')
+            get_csv(val)
+    print()
+
+
+if os.path.exists(local_database):
+    download_archives()
+else:
+    download_file(master_list)
+    download_archives()
--- translate.py
+++ translate.py
@@ -3,15 +3,13 @@
 from cgitb import text
 import requests
 import csv
-
+import json
 
 f = csv.reader(open('extracted_text.csv'), delimiter='\t')
 
 # texts = []
 # for i in f:
 #     texts.append(i[2])
-
-csvwriter = csv.writer(file, delimiter="\t")
 
 
 with open("translated_text.csv", 'w') as file:
@@ -22,11 +20,32 @@ with open("translated_text.csv", 'w') as file:
             'source': 'auto',
             'target': 'en'
         }).text
-        title = requests.post("http://localhost:5000/translate", json={
-            'q': [1],
-            'source': 'auto',
-            'target': 'en'
-        }).text
+        # title = requests.post("http://localhost:5000/translate", json={
+        #     'q': [1],
+        #     'source': 'auto',
+        #     'target': 'en'
+        # }).text
+
+
+        # print(body)
+        # if body == "\n" or title == "":
+        #     break
+            # if "translatedText" in json.loads(str(title)):
+            #     #title = json.loads(title)["translatedText"] 
+            #     pass
+        # else:
+        #     body = "none"
+
+        try:
+            body = json.loads(body)
+            if "translatedText" in body:
+                body = body["translatedText"]             
+        except ValueError:  # includes simplejson.decoder.JSONDecodeError
+            continue
+            print('Decoding JSON has failed')
+
+        if "{'error':" not in str(body):
+            csvwriter.writerow([body, i[-1]])
         
-        csvwriter.writerow([title, body, i[-1]])
+        print(body)
         # print(r.text)
