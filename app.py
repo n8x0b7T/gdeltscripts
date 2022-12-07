162ad97e3deac5669ef7d1e5182bead408917476
n8x0b7T <deretilium@protonmail.com>
2022-12-07 15:40:25 -0500

    tabs and wordclouds

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from spacy import displacy
 from nltk.corpus import stopwords
 import re
 import argparse
+from wordcloud import WordCloud
+from matplotlib import pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i',
@@ -53,15 +55,14 @@ def load_sa():
 sa = load_sa()
 
 
-def highlight_text(text, verbs):
+def highlight_text(text, words, tag):
     matches = []
-
-    for i in verbs:
+    text = text.lower()
+    for i in words:
         for item in re.finditer(i, text):
             match = {}
             match['start'], match['end'] = item.span()
-            # The tag/label that you would like to display
-            match['label'] = 'VRB'
+            match['label'] = tag
             matches.append(match)
     return matches
 
@@ -70,6 +71,33 @@ def get_verbs(doc):
     verbs = set([i.text for i in doc if i.pos_ == "VERB"])
     verbs = [i for i in verbs if str(i).lower() not in stop_words]
     return verbs
+
+
+def get_wordcloud(text):
+    wordcloud = WordCloud(width=1500, height=800,
+                          background_color="#0e1117").generate(text)
+    fig, ax = plt.subplots()
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.figure(facecolor='k')
+    plt.tight_layout(pad=0)
+    fig.padding = 0
+    # plt.gcf().set_facecolor('red')
+    # plt.margins(0)
+    plt.tight_layout(pad=0)
+    for pos in ['right', 'top', 'bottom', 'left']:
+        plt.gca().spines[pos].set_visible(False)
+
+    # Selecting the axis-X making the bottom and top axes False.
+    plt.tick_params(axis='x', which='both', bottom=False,
+                    top=False, labelbottom=False)
+
+    # Selecting the axis-Y making the right and left axes False
+    plt.tick_params(axis='y', which='both', right=False,
+                    left=False, labelleft=False)
+    for item in [fig, ax]:
+        item.patch.set_visible(False)
+    return fig
 
 
 df = pd.read_csv(args.input)
@@ -95,40 +123,55 @@ elif sentiment[0]['label'] == "positive":
     sentiment_color = "green"
 else:
     sentiment_color = "inherit"
-
 sentiment_text = f"<p style=\"margin:0;opacity:0.8\">Sentiment</p><h4 style=\"padding-top:0\">{int(sentiment[0]['score']*100)}% <span style=\"color:{sentiment_color}\">{sentiment[0]['label']}</h4>"
-st.write(sentiment_text, unsafe_allow_html=True)
 
-st.subheader(cur_row['title_tr'])
 
 doc = nlp(cur_row['body_tr'])
 highlighted_text = displacy.parse_ents(doc)
-accepted_labels = ["DATE", "EVENT", "FAC", "GPE", "LAW", "LOC", "NORP", "ORG", "PERSON", "PRODUCT", "TIME"]
-print(highlighted_text['ents'])
-highlighted_text['ents'] = [i for i in highlighted_text['ents'] if i['label'] in accepted_labels]
+accepted_labels = ["DATE", "EVENT", "FAC", "GPE", "LAW",
+                   "LOC", "NORP", "ORG", "PERSON", "PRODUCT", "TIME"]
+highlighted_text['ents'] = [
+    i for i in highlighted_text['ents'] if i['label'] in accepted_labels]
 verbs = get_verbs(doc)
-highlighted_text['ents'] += highlight_text(cur_row['body_tr'], verbs)
+highlighted_text['ents'] += highlight_text(cur_row['body_tr'], verbs, "VRB")
 
-# print(displacy.parse_ents(doc))
-ent_html = displacy.render(
-    highlighted_text, style='ent', manual=True)
-# Display the entity visualization in the browser:
-st.markdown(ent_html, unsafe_allow_html=True)
+keywords = ["demand", "demands", "uprising", "uprising", "demonstrations",
+            "demonstrations", "protest", "protests", "corruption", "reform", "violence"]
+highlighted_text['ents'] += highlight_text(
+    cur_row['body_tr'], keywords, "KWRD")
 
-# choice = st.button('Yes')
-# st.button('No')
 
-st.write("<br>", unsafe_allow_html=True)
+highlighted_text = displacy.render(
+    highlighted_text, style='ent', manual=True, options={"colors": {"KWRD": "#13f4ad"}})
+
+# title
+st.subheader(cur_row['title_tr'])
+
+
+tab1, tab2 = st.tabs(["Text", "WordCloud"])
+with tab1:
+    st.markdown(highlighted_text, unsafe_allow_html=True)
+with tab2:
+    st.pyplot(get_wordcloud(cur_row['body_tr']))
+
+# sentiment
+st.write(sentiment_text, unsafe_allow_html=True)
+
+# radio selection
 with st.form("label_form", clear_on_submit=True):
-    option = st.radio("Protest", options=('-', 'Yes', 'No', 'Trash'), horizontal=True)
+    option = st.radio("Protest", options=(
+        '-', 'Yes', 'No', 'Trash'), horizontal=True)
     st.form_submit_button("Label")
 
-
 if option == "Yes":
-    st.write("YUH")
+    pass
 elif option == "No":
     pass
+elif option == "Trash":
+    pass
 else:
-    st.write("nah")
+    pass
 
+
+# update index
 st.session_state.num += 1
