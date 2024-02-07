6d1875dca1c345010af51638c4c381615176f7ca
deretilium@protonmail.com <deretilium@protonmail.com>
2024-02-07 13:25:34 -0500

    send it

--- getdomainstats.py
+++ getdomainstats.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 df = pd.read_csv("everything/extracted.csv")
 
-m = df['SOURCEURL'].str.extract('(?<=http://)(.*?)(?=/)|(?<=https://)(.*?)(?=/)')
+m = df['SOURCEURL'].str.extract(
+    '(?<=http://)(.*?)(?=/)|(?<=https://)(.*?)(?=/)')
 m = m[0].fillna(m[1]).fillna(df['SOURCEURL'])
 
 df['DOMAINNAME'] = m
